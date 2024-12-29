45c2f63be6f6b0c9eba6eeb9aa8b4ffa929e6f63
Oleksandr Shvets <shvec.olx@gmail.com>
2024-12-29 20:20:29 +0100

    add db\models\Session.js, add rout aust/login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,7 +5,29 @@ export const registerController = async (req, res) => {
 
   res.status(201).json({
     status: 201,
-    message: 'uccessfully registered a user!',
+    message: 'Successfully registered a user!',
     data,
+  });
+};
+
+export const loginController = async (req, res) => {
+  const session = await authServices.loginUser(req.body);
+
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: session.refreshTokenValidUntil,
+  });
+
+  res.cookie('sessionId', session.id, {
+    httpOnly: true,
+    expires: session.refreshTokenValidUntil,
+  });
+
+  res.status(200).json({
+    status: 200,
+    massage: 'Successfully logged in an user!',
+    data: {
+      accessToken: session.accessToken,
+    },
   });
 };
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,54 @@
+import createHttpError from 'http-errors';
+
+import bcrypt from 'bcrypt';
+
+import { randomBytes } from 'crypto';
+
 import { UserCollection } from '../db/models/User.js';
+import { SessionColoction } from '../db/models/Session.js';
+
+import {
+  accessTokenLifeTime,
+  refreshTokenLifeTime,
+} from '../constants/users.js';
 
 export const registerUser = async (payload) => {
-  const newUser = await UserCollection.create(payload);
+  const { email, password } = payload;
+  const user = await UserCollection.findOne({ email });
+  if (user) {
+    throw createHttpError(409, 'Email in use');
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await UserCollection.create({
+    ...payload,
+    password: hashPassword,
+  });
 
   return newUser;
+};
+
+export const loginUser = async ({ email, password }) => {
+  const user = await UserCollection.findOne({ email });
+  if (!user) {
+    throw createHttpError(401, 'Email or password invalid');
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw createHttpError(401, 'Email or password invalid');
+  }
+
+  await SessionColoction.deleteOne({ userId: user._id });
+
+  const accessToken = randomBytes(30).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
+
+  return SessionColoction.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: Date.now() + accessTokenLifeTime,
+    refreshTokenValidUntil: Date.now() + refreshTokenLifeTime,
+  });
 };
